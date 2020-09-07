36020dd278444ae3c7a42c5e6acbc789fba69acb
Tianyu Li <tonylitianyu@gmail.com>
2020-09-07 11:12:52 -0500

    add gif

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import pygame
 from visual import Grid
 from a_star import A_Star
 
-n_block = 15
+n_block = 10
 origin = [0,0]
 target = [n_block-3, n_block-5]
-obstacle = [[2,3],[3,3],[4,3],[5,3],[6,3],[7,3],[8,3],[10,10]]
+obstacle = [[2,3],[3,3],[4,3],[5,3],[6,3],[7,3],[8,3]]
 finalPath = []
 
 
 grids = Grid(windowSize=400,n_block=n_block, origin=origin, target=target, obstacle=obstacle)
 agent = A_Star(origin=origin, target=target, n_block=n_block, obstacle=obstacle)
 
-
+pygame.time.wait(5000)
 while True:
     grids.drawGrid()
     done,result,visited = agent.nextStep()
@@ -25,7 +25,7 @@ while True:
         finalPath = result
     grids.drawPath(finalPath)
 
-    pygame.time.wait(200)
+    pygame.time.wait(100)
 
  
     for event in pygame.event.get():
